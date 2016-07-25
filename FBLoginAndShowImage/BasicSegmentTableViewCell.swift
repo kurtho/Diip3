ecda5af757b7c14082974a0e7af45307b5c5631c
kurt ho <ahopdanzer@gmail.com>
2016-07-25 15:06:02 +0800

    add segment control

---
+++ FBLoginAndShowImage/BasicSegmentTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  BasicSegmentTableViewCell.swift
+//  FBLoginAndShowImage
+//
+//  Created by KurtHo on 2016/7/25.
+//  Copyright © 2016年 Kurt. All rights reserved.
+//
+
+import UIKit
+
+class BasicSegmentTableViewCell: UITableViewCell {
+    @IBOutlet weak var label: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FBLoginAndShowImage/SetQuestionViewController.swift
@@ -0,0 +1,61 @@
+//
+//  SetQuestionViewController.swift
+//  FBLoginAndShowImage
+//
+//  Created by KurtHo on 2016/7/25.
+//  Copyright © 2016年 Kurt. All rights reserved.
+//
+
+import UIKit
+
+class SetQuestionViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var mySegment: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    let cellValue = ["我最吸引人的地方是","我喜歡與什麼樣的人相處"]
+    let collectionValue = [""]
+    var segmentNum = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func segmentSelect() {
+        switch mySegment.selectedSegmentIndex {
+        case 0:
+
+            break
+        case 1:
+            
+            break
+        default:
+            break
+        }
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellValue.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCellWithIdentifier("myCell", forIndexPath: indexPath)
+        cell.textLabel?.text = cellValue[indexPath.row]
+        return cell
+    }
+    
+
+    
+    
+    
+}
